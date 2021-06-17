9ab0180126883c157dc98339288408bc9d272150
Dr. Sinisa <sinisa.dragojlov@gmx.us>
2021-06-17 06:58:45 +0200

    update to node v14

--- bin/gmtasks.js
+++ bin/gmtasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-process.stdout.write("gunmetal v1.2.8\r");
+process.stdout.write("gunmetal v1.3.0\r");
 process.env.GULP_PROJECTS_DIR = process.cwd();
 process.chdir(require('gunmetal').dirname);
 require('gulp-cli')();
